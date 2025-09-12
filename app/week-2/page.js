60a38e21cfe8500892aa0f8424a999939530e630
Enzodabenzo123 <enzo.campanatorres@edu.sait.ca>
2025-09-12 16:46:51 -0600

    first commit

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import StudentInfo from "./student-info";
+
+export default function Page(){
+    return(
+        <main>
+            <h1 className="text-pink bg-yellow-700">Shopping List</h1>
+            <StudentInfo/>
+        </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+    return (
+        <main>
+            <h2 className = "font-bold text-4x1">Enzo Campana Torres</h2>
+            <Link href="https://github.com/Enzodabenzo123/cprg306-assignments">https://github.com/Enzodabenzo123/cprg306-assignments</Link>
+        </main>
+    );    
+}
